d28283bb2d2eeae7cbf117e61b3944644e111690
cpk1 <cpk1@pacbell.net>
2017-11-27 11:13:46 -0800

    add bar chart

--- samples/currentDataBWMon.R
+++ samples/currentDataBWMon.R
@@ -7,3 +7,12 @@ str(hosts)
 
 #List column names from HostStats
 #names(hosts)
+
+# show the current host CPU utilization in a bar chart.
+# first, set a wide left margin so that the host names are not truncated.
+par(mai=c(1,2,1,1))
+barplot(hosts$userPerCentCpu,names=hosts$hostname,horiz=TRUE,main="Host %CPU",las=1)
+
+
+# try the following to more easily view your host data:
+edit(hosts)
